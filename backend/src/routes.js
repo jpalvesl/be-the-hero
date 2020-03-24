c7a8ef972428743815113a2cb2f5b1f0bd04a260
João Lima <jpalvesl101@gmail.com>
2020-03-24 14:02:01 +0000

    connection with db and routes, route ongs to register created

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-    res.send({ok: true})
+routes.post('/ongs', (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body
+
+  const id = crypto.randomBytes(4).toString('HEX')
+
+  return res.json({ id })
 })
 
 module.exports = routes;
