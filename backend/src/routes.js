b9bd8f2d6ca246ad92b8bb688c14981fa9e486a7
João Lima <jpalvesl101@gmail.com>
2020-03-24 17:02:05 +0000

    route to login autenticate from ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = Router()
 
@@ -14,5 +15,7 @@ routes.get('/incidents', IncidentController.index)
 routes.delete('/incidents/:id', IncidentController.delete)
 
 routes.get('/profile', ProfileController.index)
+
+routes.post('/sessions', SessionController.create)
 
 module.exports = routes;
