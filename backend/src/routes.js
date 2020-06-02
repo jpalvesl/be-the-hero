810a2c6a2908f46e0aa2524547a7279c24800fde
João Lima <jpalvesl101@gmail.com>
2020-06-02 02:36:53 +0000

    criando validaçao nas rotas restantes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,31 +13,45 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.string().required().min(10).max(11),
+    whatsapp: Joi.string().required().min(13).max(14),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2)
   })
 }), OngController.create)
 
-routes.post('/incidents', IncidentController.create)
+routes.post('/incidents', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+      title: Joi.string(),
+      description: Joi.string(),
+      value: Joi.number(),
+  }),
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown()
+}), IncidentController.create)
+
 routes.get('/incidents', celebrate({
   [Segments.QUERY]: Joi.object().keys({
-    page: Joi.number()
+    page: Joi.number(),
   })
 }), IncidentController.index)
 
 routes.delete('/incidents/:id', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
-    id: Joi.number().required()
+    id: Joi.number().required(),
   })
 }), IncidentController.delete)
 
 routes.get('/profile', celebrate({
   [Segments.HEADERS]: Joi.object({
-    authorization: Joi.string().required()
+    authorization: Joi.string().required(),
   }).unknown()
 }), ProfileController.index)
 
-routes.post('/sessions', SessionController.create)
+routes.post('/sessions', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string()
+  })
+}), SessionController.create)
 
 module.exports = routes;
